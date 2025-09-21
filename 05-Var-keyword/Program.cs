892f87d09a08f6064130964936e239a0fef30e69
Pavan Varma <pavanvarma058@gmail.com>
2025-09-21 23:26:57 +0100

    Learned about var keyword in C#

--- 05-Var-keyword/Program.cs
+++ 05-Var-keyword/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,28 @@ namespace _05_Var_keyword
     {
         static void Main(string[] args)
         {
+            // var keyword
+            var age = 23;
+            Console.WriteLine(age);
+
+            var bigNum = 90000000000L;
+            Console.WriteLine(bigNum);
+
+            var negative = -55.2D;
+            Console.WriteLine(negative);
+
+            var precision = 5.000001F;
+            Console.WriteLine(precision);
+
+            var money = 14.99M;
+            Console.WriteLine(money); 
+
+            var name = "Pavan";
+            Console.WriteLine(name);
+
+            var letter = 'A';
+            Console.WriteLine(letter);
+
             Console.ReadLine();
         }
     }
