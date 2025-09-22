b292bdda1a33912a7f1ed1a6f717fda30e990335
Pavan Varma <pavanvarma058@gmail.com>
2025-09-22 22:38:53 +0100

    Learned about const keyword in C#

---
+++ 06-Const-keyword/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Const_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // const keyword
+            const int num = 100;
+            Console.WriteLine(num);
+
+            Console.ReadLine();
+        }
+    }
+}
