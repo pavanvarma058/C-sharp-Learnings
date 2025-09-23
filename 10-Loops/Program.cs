2b117ee09247a57733bd439192e263bf09e77308
Pavan Varma <pavanvarma058@gmail.com>
2025-09-23 23:04:09 +0100

    Learned about for,while,do-while loops

---
+++ 10-Loops/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Loops
+
+            // for loop
+            for (int i = 1; i <= 10; i++) {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            // while loop
+            int j = 1;
+
+            while(j <= 10)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            Console.WriteLine();
+            
+            // do while loop
+            int k = 1;
+
+            do
+            {
+                Console.WriteLine(k);
+            }while(k != 1);
+
+
+            // Condition ? True : False;
+            int age = 18;
+            Console.WriteLine(age >= 18 ? "Valid" : "Invalid");
+
+            Console.ReadLine();
+        }
+    }
+}
