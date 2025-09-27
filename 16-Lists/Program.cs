2d93ad1cfb65f4d0f18407b69eb5392d9ede93e3
Pavan Varma <pavanvarma058@gmail.com>
2025-09-27 23:13:03 +0100

    Learned about lists

--- 16-Lists/Program.cs
+++ 16-Lists/Program.cs
@@ -10,6 +10,35 @@ namespace _16_Lists
     {
         static void Main(string[] args)
         {
+            // lists -> dynamic arrays
+            List<int> numbers = new List<int>();
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+
+            // some methods in lists
+            numbers.Remove(3); // remove by value
+
+            numbers.RemoveAt(0); // remove by index
+
+            foreach(int n in numbers)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(numbers.Contains(5));
+
+            Console.ReadLine();
         }
     }
 }
