08aff7b99a5247058560ed2094b18de0a292eb27
Pavan Varma <pavanvarma058@gmail.com>
2025-09-29 22:41:02 +0100

    Learned about functions in C#

---
+++ 19-Functions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            helloMessage("Yeswanth");
+            helloMessage("Zoro");
+            Console.WriteLine();
+            int result = add();
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        // Functions
+        static void helloMessage(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+
+        // Return type functions
+        static int add()
+        {
+            return 5 + 5;
+        }
+    }
+}
